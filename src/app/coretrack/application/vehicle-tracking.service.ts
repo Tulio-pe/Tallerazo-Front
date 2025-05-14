67a685ced20132f0510a0fdf93ad1a9522e30630
Mitawarmi2000 <u202214130@upc.edu.pe>
2025-05-14 09:09:52 -0500

    refactor: delete type import.

--- src/app/coretrack/application/vehicle-tracking.service.ts
+++ src/app/coretrack/application/vehicle-tracking.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core"
 import type { Observable } from "rxjs"
 import type { VehicleTracking } from "../domain/models/vehicle-tracking.model"
-import type { VehicleTrackingRepository } from "../domain/repositories/vehicle-tracking.repository"
+import  { VehicleTrackingRepository } from "../domain/repositories/vehicle-tracking.repository"
 
 @Injectable({
   providedIn: "root",
