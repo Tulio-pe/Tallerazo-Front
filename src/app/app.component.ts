b8f4ca3aeb83dcb5bc986bba790142647e6597a9
Mitawarmi2000 <u202214130@upc.edu.pe>
2025-05-16 12:13:39 -0500

    feat(app): added toolbar routes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core"
 import { RouterOutlet } from "@angular/router"
+import {ToolbarComponent} from './public/components/toolbar/toolbar.component';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolbarComponent],
 })
 export class AppComponent {
   title = "vehicle-tracking"
