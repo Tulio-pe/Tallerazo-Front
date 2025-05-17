7a583323c56132e5032daffcfddf518f6d96069d
Jesús Castillo Vidal <jcdev2004@gmail.com>
2025-05-17 13:45:46 -0500

    feat: list item already done and submitted with fake data

--- src/app/workshop-discovery/components/workshop-item/workshop-item.component.ts
+++ src/app/workshop-discovery/components/workshop-item/workshop-item.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+export interface Workshop {
+  id: string;
+  name: string;
+  address: string;
+  telephone: string;
+  description: string;
+  imageUrl?: string;
+}
 @Component({
   selector: 'app-workshop-item',
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './workshop-item.component.html',
   styleUrl: './workshop-item.component.css'
 })
 export class WorkshopItemComponent {
-
+  @Input() workshop!: Workshop;
+  @Output() view = new EventEmitter<string>();
 }
--- src/app/workshop-discovery/components/workshop-list/workshop-list.component.ts
+++ src/app/workshop-discovery/components/workshop-list/workshop-list.component.ts
@@ -1,11 +1,67 @@
+import {WorkshopItemComponent, Workshop} from "../workshop-item/workshop-item.component";
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-workshop-list',
-  imports: [],
+    imports: [
+        WorkshopItemComponent,
+      CommonModule,
+    ],
   templateUrl: './workshop-list.component.html',
   styleUrl: './workshop-list.component.css'
 })
 export class WorkshopListComponent {
+  constructor(private router: Router) {}
+
+  workshops: Workshop[] = [
+    //  Ejemplo de datos
+    {
+      id: '1',
+      name: 'Nombre de la empresa',
+      address: 'Av. Siempre Viva 123',
+      telephone: '(01) 123-4567',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat...',
+    },
+    {
+      id: '2',
+      name: 'Nombre de la empresa',
+      address: 'Av. Siempre Viva 123',
+      telephone: '(01) 123-4567',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat...',
+    },
+    {
+      id: '3',
+      name: 'Nombre de la empresa',
+      address: 'Av. Siempre Viva 123',
+      telephone: '(01) 123-4567',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat...',
+    },
+    {
+      id: '4',
+      name: 'Nombre de la empresa',
+      address: 'Av. Siempre Viva 123',
+      telephone: '(01) 123-4567',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat...',
+    },
+    {
+      id: '5',
+      name: 'Nombre de la empresa',
+      address: 'Av. Siempre Viva 123',
+      telephone: '(01) 123-4567',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat...',
+    },
+    {
+      id: '6',
+      name: 'Nombre de la empresa',
+      address: 'Av. Siempre Viva 123',
+      telephone: '(01) 123-4567',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat...',
+    },
+
+  ];
+  goToDetail(id: string) {
+    this.router.navigate(['/workshop', id]);
+  }
 
 }
